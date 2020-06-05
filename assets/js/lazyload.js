7a59d258e936f83854efae1779307519d5ac4816
limjh16 <limjh16@gmail.com>
2020-06-05 16:20:03 +0800

    use liquid tags to lazyload img and iframe
    the js file was pretty dumb since we could have just made the attribute static
    
    and anyways the js file broke on prettier html formatting so

--- assets/js/lazyload.js
+++
@@ -1,4 +0,0 @@
-var images = document.getElementsByTagName('img'); 
-for(var i = 0; i < images.length; i++) images[i].setAttribute("loading", "lazy");
-var iframes = document.getElementsByTagName('iframe');
-for(var i = 0; i < iframes.length; i++) iframes[i].setAttribute("loading", "lazy");
